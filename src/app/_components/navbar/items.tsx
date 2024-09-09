58193f51940a80f03fcdfd447b8e423f6cba4f43
Aman <aman30t@gmail.com>
2024-09-09 23:22:17 +0530

    menu items click

--- src/app/_components/navbar/items.tsx
+++ src/app/_components/navbar/items.tsx
@@ -3,27 +3,29 @@ import Link from 'next/link'
 import Logo from './logo'
 
 type itemsProps = {
-    checker : boolean
+    checker: boolean,
+    isOpen?: boolean,
+    onClick?: () => void // New prop for handling item click
 }
 
-const Items = ({checker}:itemsProps)=>{
+const Items = ({ checker, isOpen, onClick }: itemsProps) => {
     return (
         <div className={`px-4 py-2 ${checker ? 'hidden sm:flex flex-row items-center gap-5' : 'flex flex-col gap-[40px] z-20'}`}>
             {checker ? '' : <Logo label='/InCb.png' />}
-            <div className='hover:underline text-sm font-semibold border-b-[1px]'>
-               <Link href='/'>Home</Link>
-            </div >
-            <div className='hover:underline text-sm font-semibold   border-b-[1px]'>
-               <Link href='/influencers'>Influencers</Link>
+            <div className='hover:underline text-sm font-semibold border-b-[1px]' onClick={onClick}>
+                <Link href='/'>Home</Link>
             </div>
-            <div className='hover:underline text-sm font-semibold   border-b-[1px]'>
-               <Link href='/socialmedia'>Social Media</Link>
+            <div className='hover:underline text-sm font-semibold border-b-[1px]' onClick={onClick}>
+                <Link href='/influencers'>Influencers</Link>
             </div>
-            <div className='hover:underline text-sm font-semibold   border-b-[1px]'>
-               <Link href='/brands'>Brands</Link>
+            <div className='hover:underline text-sm font-semibold border-b-[1px]' onClick={onClick}>
+                <Link href='/socialmedia'>Social Media</Link>
             </div>
-            <div className='hover:underline text-sm font-semibold  border-b-[1px]'>
-               <Link href='/influencer-marketing'>Influencer Marketing</Link>
+            <div className='hover:underline text-sm font-semibold border-b-[1px]' onClick={onClick}>
+                <Link href='/brands'>Brands</Link>
+            </div>
+            <div className='hover:underline text-sm font-semibold border-b-[1px]' onClick={onClick}>
+                <Link href='/influencer-marketing'>Influencer Marketing</Link>
             </div>
         </div>
     )
--- src/app/_components/navbar/navbar.tsx
+++ src/app/_components/navbar/navbar.tsx
@@ -1,46 +1,40 @@
-"use client"
+'use client'
 import { RxCross2 } from "react-icons/rx";
 import Logo from "./logo";
 import Items from "./items";
 import Brands from "./brands";
 import { RiMenu3Fill } from "react-icons/ri";
-import {useState} from 'react';
+import { useState } from 'react';
 
 const Navbar = () => {
-  const [isOpen,setIsOpen] = useState<boolean>(false)
-  const handle = ():void=>{
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  
+  const handle = (): void => {
     setIsOpen(!isOpen);
-  }
-    return ( 
+  };
+
+  return (
     <>
-   
-    <div className='fixed border-b-[1px] shadow w-full h-[85px] bg-white text-black py-5 px-2 z-20'>
-     
+      <div className='fixed border-b-[1px] shadow w-full h-[85px] bg-white text-black py-5 px-2 z-20'>
         <div className='container flex flex-row justify-between items-center'>
           {isOpen ? <h1></h1> : <div className=''><Logo label='/InCb.png' /></div>} 
-          <Items checker={true} />
+          <Items isOpen={isOpen} checker={true} onClick={handle} />
           <Brands />
-         
           <div onClick={handle} className='sm:hidden'>
-             <RiMenu3Fill size={34}/>
+            <RiMenu3Fill size={34} />
           </div>
-          
-          </div>   
-      
-
-     
+        </div>   
       </div>
       
- {/* Sliding Menu */}
- <div className={`fixed px-6 py-6 flex flex-col gap-[20px] z-50 top-0 right-0 h-full w-[75%] max-w-sm bg-white shadow-lg transform ${isOpen ? 'translate-x-0' : `translate-x-full`} transition-transform duration-300 ease-in-out`}>
+      {/* Sliding Menu */}
+      <div className={`fixed px-6 py-6 flex flex-col gap-[20px] z-50 top-0 right-0 h-full w-[75%] max-w-sm bg-white shadow-lg transform ${isOpen ? 'translate-x-0' : `translate-x-full`} transition-transform duration-300 ease-in-out`}>
         <div className='p-6'>
-           <div onClick={handle} className='absolute top-5 left-5'><RxCross2 /></div>
-           <Items checker={false} />
+          <div onClick={handle} className='absolute top-5 left-5'><RxCross2 /></div>
+          <Items checker={false} onClick={handle} />
         </div>
       </div>
-      {/* </div> */}
-    </> 
-    );
+    </>
+  );
 }
- 
+
 export default Navbar;
